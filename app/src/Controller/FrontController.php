a48f62a95d3382c0ad1072387e048126b544b308
rokas.lakstauskas <rokas.lakstauskas@gmail.com>
2021-07-10 22:27:29 +0300

    webpack added readme update front controller more packages

--- app/src/Controller/FrontController.php
+++ app/src/Controller/FrontController.php
@@ -12,7 +12,7 @@ class FrontController extends AbstractController
     public function index(): Response
     {
         return $this->render('front/index.html.twig', [
-            'controller_name' => 'FrontController',
+            'words' => ['a', 'bb'],
         ]);
     }
 }
